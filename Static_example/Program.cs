7b564f3e2f28012d40fefc3cb0aba8008c336fae
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:09:01 +0200

    Add project files.

---
+++ Static_example/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Threading.Channels;
+namespace Static_example
+{
+
+    class Peogram
+    {
+        static void Main(string[] args)
+        {
+            double r = Math.Round(35.5);
+            Console.WriteLine(r);
+            Car.Accelerate();
+            Car car = new Car();
+            car.SlowDown();
+        }
+        public class Car
+        {
+
+            public static void Accelerate() 
+            {
+                Console.WriteLine("car is speeding up");
+
+            }
+            public void SlowDown()
+            {
+                Console.WriteLine("car is slowing down ");
+
+            }
+        }
+
+    }
+
+}
+ 
